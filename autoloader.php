3a691c3192c5c998bb7bae8e8e9997f73569dd06
AVillalonga <adrien.villalonga@gmail.com>
2021-11-30 19:58:53 +0100

    0.0.02

--- autoloader.php
+++ autoloader.php
@@ -7,23 +7,27 @@
 namespace WPMVC;
 
 if (!function_exists('wpmvc_autoload')) {
-    function wpmvc_autoload($resource = '')
+    function wpmvc_autoload(string $resource = ''): bool
     {
-        if(empty($resource)) return;
-        
-        $remove_limit_antislashs    = fn($resource): string => trim($resource, '\\');
-        $got_namespace              = fn($namespace, $resource): string => strpos($resource, WPB_NAMESPACE) === 0;
-        $remove_namespace           = fn($namespace, $resource): string => preg_replace('/'.preg_quote($namespace, '/').'/', '', $resource, 1);
-        
+        if (empty($resource)) return false;
+
+        $remove_limit_antislashs    = fn (string $resource): string => trim($resource, '\\');
+        $got_namespace              = fn (string $namespace, string $resource): string => strpos($resource, WPB_NAMESPACE) === 0;
+        $remove_namespace           = fn (string $namespace, string $resource): string => preg_replace('/' . preg_quote($namespace, '/') . '/', '', $resource, 1);
+
         $resource = $remove_limit_antislashs($resource);
-        if($got_namespace(WPMVC_THEME_NAMESPACE, $resource)) {
+        if ($got_namespace(WPMVC_THEME_NAMESPACE, $resource)) {
             $resource = $remove_namespace(WPMVC_THEME_NAMESPACE, $resource);
             $resource = str_replace('\\', '/', $resource);
             $resource = str_replace('_', '-', $resource);
             $resource = strtolower($resource);
             $resource = WPMVC_THEME_DIRECTORY .  '/' . $resource . '.php';
-            if(is_file($resource)) require_once $resource;
+            if (is_file($resource)) {
+                require_once $resource;
+                return true;
+            }
         }
+        return false;
     }
 }
 
--- di.php
+++ di.php
@@ -5,3 +5,57 @@
  */
 
 namespace WPMVC;
+
+use ReflectionClass;
+use ReflectionObject;
+
+if (!class_exists("StaticDependencyInjector")) {
+    class StaticDependencyInjector
+    {
+        private static array $__dependencies = [];
+
+        public static function get_instance(string $class): object|callable { return self::$__dependencies[$class]; }
+        
+        public static function autoload(string $class): object|callable {
+            $reflection = new \ReflectionClass(new $class());
+            $constructor = $reflection->getConstructor();
+            $constructor_parameters = $constructor !== null ? $constructor->getParameters() : [];
+            foreach($constructor_parameters as $i=>$cp) {
+                if(($cp->getType() === null) && (!$cp->isOptional())) {
+                    throw new \Error('Missing Type "' . $constructor_parameters[$i]->getName() . '" in class "' . $class . '"');
+                }
+            }
+            $constructor_args = self::__load_args($constructor_parameters);
+        }
+
+        private static function __load_args(array $parameters) {
+            return array_map() /** @todo : OK */
+        }
+
+        private static function __has_trait(\ReflectionClass $reflection, string $trait_name): bool {
+            return in_array($trait_name, $reflection->getTraitNames()); 
+        }
+    }
+}
+
+
+if (!trait_exists('Dependency')) {
+    trait Dependency
+    {
+        public static function get_instance(...$args): object
+        {
+            $class = get_called_class();
+            $instance = StaticDependencyInjector::get_instance('SINGLETON', $class);
+            if($instance !== null) return is_callable($instance) ? $instance() : $instance;
+            else $instance = StaticDependencyInjector::autoload($class);
+        }
+    }
+}
+
+if (!trait_exists('Singleton')) {
+    trait Singleton { use Dependency; }
+}
+
+if (!trait_exists('Transient')) {
+    trait Transient { use Dependency; }
+}
